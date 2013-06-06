4a54e6d9ae502feb073256e7d8db37ab26ad2e8c
Daniel Morrison <daniel@collectiveidea.com>
2013-06-05 20:47:09 -0700

    rails new disorder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disorder::Application.config.secret_token = '6d8bae8827450c8344b0ee45e772c589bf50e3c0fe8d436b1a373d2e8b2c497b7f2e5e33b9a98828cd9d6ed2d656f1cd75374f988947947f200857e77a571bab'
